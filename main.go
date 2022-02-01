43ac0541d3c40bb79838a9e2451d397fd64d145c
Brian Jing <brjing@seek.com.au>
2022-02-02 01:38:24 +1100

    Split things into functions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 	"strings"
 )
 
+// Geonames location data type
 type Location struct {
   geonameid        int 
   name             string
@@ -33,13 +35,14 @@ type Location struct {
 } 
 
 func fromStringToInt(s string) int {
+  defaultValue := 0
   if s == "" {
-    return 0
+    return defaultValue
   } else {
     res, err := strconv.Atoi(s)
     if err != nil {
       fmt.Println(err)
-      os.Exit(2)
+      return defaultValue
     }
 
     return res
@@ -47,78 +50,102 @@ func fromStringToInt(s string) int {
 }
 
 func fromStringToFloat(s string) float64 {
+  defaultValue := 0.0
   if s == "" {
-    return 0.0
+    return defaultValue
   } else {
     res, err := strconv.ParseFloat(s, 64)
     if err != nil {
       fmt.Println(err)
-      os.Exit(2)
+      return defaultValue
     }
     return res
   }
 }
 
-func main() {
+/*
+** Convert a csv record into a Geonames Location type
+*/
+func handleRecord(record []string) Location {
+  return Location {
+    geonameid        : fromStringToInt(record[0]), 
+    name             : record[1],  
+    asciiname        : record[2],  
+    alternatenames   : strings.Split(record[3], ","),  
+    latitude         : fromStringToFloat(record[4]),  
+    longitude        : fromStringToFloat(record[5]),  
+    featureClass     : record[6],  
+    featureCode      : record[7],  
+    countryCode      : record[8],  
+    cc2              : strings.Split(record[9], ","),  
+    admin1Code       : record[10],  
+    admin2Code       : record[11],  
+    admin3Code       : record[12],  
+    admin4Code       : record[13],  
+    population       : fromStringToInt(record[14]),  
+    elevation        : fromStringToInt(record[15]),  
+    dem              : fromStringToInt(record[16]),  
+    timezone         : record[17],  
+    modificationDate : record[18], 
+  }
+}
+
+/*
+** Get a list of data files
+*/
+func getDataFiles() []fs.FileInfo {
+    // Get all the file names from data/
   files, err := ioutil.ReadDir("data/")
   if err != nil {
     log.Fatal(err)
   }
+  return files
+}
 
+/*
+** Load locations from a single data file
+*/
+func loadLocationsFromDataFile(fileName string) []Location {
+  f, err := os.Open("data/" + fileName)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  r := csv.NewReader(f)
+  r.Comma = '\t'
+  r.Comment = '#'
+
+  records, err := r.ReadAll()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  locations := []Location{}
+  for _, record := range records {
+    locations = append(locations, handleRecord(record))
+  }
+  return locations
+}
+
+
+func main() {
+  dataFiles := getDataFiles()
   
   allLocations := []Location{}
-  for _, file := range files {
+  for _, file := range dataFiles {
     fmt.Println("Processing file " + file.Name())
 
     if strings.HasSuffix(file.Name(), ".txt") {
-      f, err := os.Open("data/" + file.Name())
-
-      if err != nil {
-        log.Fatal(err)
-      }
-
-      r := csv.NewReader(f)
-      r.Comma = '\t'
-      r.Comment = '#'
-
-      records, err := r.ReadAll()
-
-      if err != nil {
-        log.Fatal(err)
-      }
-
-      for _, record := range records {
-        location := Location {
-          geonameid        : fromStringToInt(record[0]), 
-          name             : record[1],  
-          asciiname        : record[2],  
-          alternatenames   : strings.Split(record[3], ","),  
-          latitude         : fromStringToFloat(record[4]),  
-          longitude        : fromStringToFloat(record[5]),  
-          featureClass     : record[6],  
-          featureCode      : record[7],  
-          countryCode      : record[8],  
-          cc2              : strings.Split(record[9], ","),  
-          admin1Code       : record[10],  
-          admin2Code       : record[11],  
-          admin3Code       : record[12],  
-          admin4Code       : record[13],  
-          population       : fromStringToInt(record[14]),  
-          elevation        : fromStringToInt(record[15]),  
-          dem              : fromStringToInt(record[16]),  
-          timezone         : record[17],  
-          modificationDate : record[18], 
-        }
-
-        allLocations = append(allLocations, location)
-      }
+      locations := loadLocationsFromDataFile(file.Name())
+      allLocations = append(allLocations, locations...)
       fmt.Println()
     } else {
       msg := file.Name() + " is not a data file" 
       fmt.Println(msg)
     }
-    fmt.Println("Total number of locations: ", len(allLocations))
-    fmt.Println("First location: ", allLocations[0])
   }
+
+  fmt.Println("Total number of locations: ", len(allLocations))
+  fmt.Println("First location: ", allLocations[0])
 }
 
