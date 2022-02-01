d5e5f245488b137a46101372d05cc5a3493a95a4
Brian Jing <brjing@seek.com.au>
2022-02-02 01:01:22 +1100

    Commit go files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello-world
+
+go 1.17
+
+require github.com/rbrahul/gofp v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Location struct {
+  geonameid        int 
+  name             string
+  asciiname        string
+  alternatenames   []string
+  latitude         float64
+  longitude        float64
+  featureClass     string
+  featureCode      string
+  countryCode      string
+  cc2              []string
+  admin1Code       string
+  admin2Code       string
+  admin3Code       string
+  admin4Code       string
+  population       int
+  elevation        int
+  dem              int
+  timezone         string
+  modificationDate string
+} 
+
+func fromStringToInt(s string) int {
+  res, err := strconv.Atoi(s)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(2)
+  }
+
+  return res
+}
+
+func fromStringToFloat(s string) float64 {
+  res, err := strconv.ParseFloat(s, 64)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(2)
+  }
+
+  return res
+}
+
+func main() {
+  files, err := ioutil.ReadDir("data/")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  for _, file := range files {
+    fmt.Println("Processing file " + file.Name())
+
+    if strings.HasSuffix(file.Name(), ".txt") {
+      f, err := os.Open("data/" + file.Name())
+
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      r := csv.NewReader(f)
+      r.Comma = '\t'
+      r.Comment = '#'
+
+      records, err := r.ReadAll()
+
+      if err != nil {
+        log.Fatal(err)
+      }
+
+
+      locations := []Location{}
+      for _, record := range records {
+        location := Location {
+          geonameid        : fromStringToInt(record[0]), 
+          name             : record[1],  
+          asciiname        : record[2],  
+          alternatenames   : strings.Split(record[3], ","),  
+          latitude         : fromStringToFloat(record[4]),  
+          longitude        : fromStringToFloat(record[5]),  
+          featureClass     : record[6],  
+          featureCode      : record[7],  
+          countryCode      : record[8],  
+          cc2              : strings.Split(record[9], ","),  
+          admin1Code       : record[10],  
+          admin2Code       : record[11],  
+          admin3Code       : record[12],  
+          admin4Code       : record[13],  
+          population       : fromStringToInt(record[14]),  
+          elevation        : fromStringToInt(record[15]),  
+          dem              : fromStringToInt(record[16]),  
+          timezone         : record[17],  
+          modificationDate : record[18], 
+        }
+
+        locations = append(locations, location)
+      }
+      fmt.Println(locations[0])
+      fmt.Println()
+    } else {
+      msg := file.Name() + " is not a data file" 
+      fmt.Println(msg)
+    }
+  }
+}
+
